e39f0711f092bde897862a26cc1f3bad0d4a83f5
Punith R B <punithrb123avs@gmail.com>
2025-02-19 19:20:37 +0530

    Task _scheduling.js  is subitted

---
+++ Task _scheduling.js
@@ -0,0 +1,7 @@
+let tasks=["Morning walk", "finish pending assessments", "volleyball", "gym", "classes"];
+tasks.splice(0,1)
+
+tasks.unshift("Read the book", "College")
+
+tasks[tasks.length-1]="practice coading"
+console.log(tasks)
