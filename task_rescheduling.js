957a615d4b5d8f6e96106f1318a50c3413e4ccbd
Punith R B <punithrb123avs@gmail.com>
2025-02-21 11:17:15 +0530

    task_rescheduling added

---
+++ task_rescheduling.js
@@ -0,0 +1,9 @@
+let tasklist=["task1", "task2", "task3", "task4", "task5"];
+
+tasklist.shift()
+
+tasklist.unshift("high preority task1", "higth preority tsak2")
+
+tasklist[tasklist.length-1]="new updated last task"
+
+console.log(tasklist)
